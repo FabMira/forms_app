98983533cd7e1ae8a6a217261bd015d3a348c645
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-03-05 01:52:24 -0300

    refactoring bloc handler functions

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -12,11 +12,19 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
 
   void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
     emit(state.copyWith(
-      counter: state.counter + event.value,
-      transactionCount: state.transactionCount + 1));
+        counter: state.counter + event.value,
+        transactionCount: state.transactionCount + 1));
   }
 
   void _onCounterReset(CounterReset event, Emitter<CounterState> emit) {
     emit(state.copyWith(counter: 0));
+  }
+
+  void increaseBy([int value = 1]) {
+    add(CounterIncreased(value));
+  }
+
+  void resetCounter() {
+    add(CounterReset());
   }
 }
